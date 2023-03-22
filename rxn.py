9ee9a3f176ed5e81415fbaf1ac18963aa2cdf0ac
ITX-SANAN <121339222+ITX-SANAN@users.noreply.github.com>
2023-03-22 22:14:03 +0500

    Update rxn.py

--- rxn.py
+++ rxn.py
@@ -2,7 +2,7 @@ import os
 import platform
 print("JOIN FB GROUP")
 os.system('clear')
-os.system("xdg-open https://facebook.com/groups/460389902592352/")
+os.system("xdg-open https://www.facebook.com/groups/207678473842318/")
 os.system("git pull")
 b = platform.architecture()[0]
 if b == '64bit':
